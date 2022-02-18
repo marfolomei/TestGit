93063c10ce91b78adb279978939aa66a53af1c36
marfolomei <66416965+marfolomei@users.noreply.github.com>
2022-02-18 14:59:06 +0500

    first commit

---
+++ portofolio/data.php
@@ -0,0 +1,75 @@
+<?php
+return [
+    'about' =>[
+        'name'=> 'Розов Александр' ,
+        'post' => 'junior',
+        'email'=> 'sash.rozoff@yandex.ru',
+        'phone'=> '+79676334722',
+        'site'=> 'vk.com',
+        'git' => 'github.com/marfolomei'
+
+
+
+
+    ],
+    'education'=>[
+        [
+            'faculty' => 'Информационные Системы и программирование',
+            'college' => 'Уксап',
+            'yearStart'=> 2020,
+            'yearFinish'=> 2024
+
+
+        ],
+        [
+
+
+            'faculty' => 'Общеобразовательная программа',
+            'college' => 'МБОУ СОШ № 75',
+            'yearStart'=> 2011,
+            'yearFinish'=> 2020
+
+        ],
+
+
+    ],
+    'language'=>[
+        'Russian', 'English'
+    ],
+    'interests'=>[
+        'Игры', 'Музыка'
+    ],
+    'aboutCareer' => "Ку Гайс, это я",
+    'career' => [
+        [
+            'post' => 'Прачечник',
+            'place'=>'Hayat Hotel, Yekaterinburg',
+           'duty' => 'Прачке работал)))',
+            'yearStart'=> 2011,
+            'yearFinish'=> 2020
+
+        ],
+        [
+            'post' => 'Junior',
+        'place'=>' Вконтакте Saint-Peterburg',
+        'duty' => 'Носил кофе, докапывал вопросами',
+            'yearStart'=> 2011,
+            'yearFinish'=> 2020
+            ],
+        [
+            'post' => 'Intern PHP',
+            'place'=>'fikali, Yekaterinburg',
+            'duty' => 'Ел на кухне',
+          'yearStart'=> 2011,
+            'yearFinish'=> 2020
+
+        ],
+
+
+    ]
+
+
+
+
+    ];
+
